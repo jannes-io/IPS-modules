2bcf1fa7d344c99be3f1108d5193b3156c5a26ce
JannesD <jannesdrijkoningen@gmail.com>
2021-07-21 13:47:35 +0200

    :pencil: add instructions to comments

--- constants.php
+++ constants.php
@@ -1,5 +1,11 @@
 <?php
 define('IN_DEV', true);
+
+/**
+ * Since PERSCOM is horribly developed and throws a ton of errors,
+ * Uncomment these 3 lines when dealing with PERSCOM code. Try to always keep them commented
+ * so that our own code is of high quality.
+ */
 //define('IN_DEV_STRICT_MODE', false);
 //define('DEV_USE_WHOOPS', false);
 //define('DEV_HIDE_DEV_TOOLS', true);
